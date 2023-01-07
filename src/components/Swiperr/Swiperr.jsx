bb0f1f032c2b8fadb34eccd193d668a5225f20ce
Med Oussama Braiek <73386969+MedOussamaBraiek@users.noreply.github.com>
2023-01-07 13:51:48 +0100

    Animation1

--- src/components/Swiperr/Swiperr.jsx
+++ src/components/Swiperr/Swiperr.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -10,23 +10,23 @@ import "swiper/css/navigation";
 import "./swiperr.css";
 
 import { Autoplay, Parallax, Pagination, Navigation } from "swiper";
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
 const Swiperr = () => {
   return (
-    <Container className='swiper__section'>
+    <Container className="swiper__section">
       <Swiper
         // direction={"vertical"}
         slidesPerView={1}
         style={{
           "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff"
+          "--swiper-pagination-color": "#fff",
         }}
         speed={600}
         loop={true}
         parallax={true}
         pagination={{
-          clickable: true
+          clickable: true,
         }}
         navigation={true}
         modules={[Autoplay, Parallax, Pagination, Navigation]}
@@ -39,48 +39,35 @@ const Swiperr = () => {
         <div
           slot="container-start"
           className="parallax-bg"
-          
           data-swiper-parallax="-23%"
         ></div>
-        <SwiperSlide
-          className='img-fluid swiper1'
-        >
-          <div className="title title1" data-swiper-parallax="-300">
-            <h1>Votre Famille</h1>
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-             Ma priorité
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide
-          className='img-fluid swiper2'
-        >
-          <div className="title title2" data-swiper-parallax="-300">
-              <h1>Vos Amis</h1>
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Ma priorité
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide
-         className='img-fluid swiper3'
-        >
+        <SwiperSlide className="img-fluid swiper3">
           <div className="title title3" data-swiper-parallax="-300">
             <h1>Vos Sourires</h1>
           </div>
           <div className="text" data-swiper-parallax="-100">
-            <p>
-              Ma priorité
-            </p>
+            <p>Ma priorité</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="img-fluid swiper1">
+          <div className="title title1" data-swiper-parallax="-300">
+            <h1>Votre Famille</h1>
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>Ma priorité</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="img-fluid swiper2">
+          <div className="title title2" data-swiper-parallax="-300">
+            <h1>Vos Amis</h1>
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>Ma priorité</p>
           </div>
         </SwiperSlide>
       </Swiper>
     </Container>
-  )
-}
+  );
+};
 
-export default Swiperr
+export default Swiperr;
